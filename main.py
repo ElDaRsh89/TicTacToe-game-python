f60cb9b55e87fd0de2b224a8e5a110a6794f4859
ElDaRsh89 <64213031+ElDaRsh89@users.noreply.github.com>
2020-05-11 20:00:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -90,6 +90,12 @@ def choice_validator(user_inp):
     return inner_number
 
 
+# There are 3 while loops:
+# The first (while yes) will keep the program running until the user doesn't want to play, only then will the program
+# end.
+# The second (while game) will keep the game running until either the user or the computer wins.
+# The third (while not cycle_finished) will keep each cycle running (the cycle is 2 choices; one for each player) until
+# both players have chosen a cell or until one of them wins.
 def play_game():
     yes = True
     while yes:
@@ -100,9 +106,10 @@ def play_game():
         while game:
             cycle_finished = False  # the cycle finished when computer puts an "O" in a cell.
             while not cycle_finished:
-                print ("         Tic Tac Toe           ")
-                print("First", new_board)  # The first board that will appear to the user. Of course an empty one. (line 88)
-                # 1) the user input will be validated by the validator funcion and the correct input will be stored in
+                print("\n", "         Tic Tac Toe           ", end="\n" * 2)
+                # The first board that will appear to the user. Of course an empty one. (line 88)
+                print("First", new_board, end="\n" * 2)
+                # 1) the user input will be validated by the validator function and the correct input will be stored in
                 # the choice variable.
                 choice = choice_validator(input("Choose a cell by entering a number from 1 to 9 \nYour Choice: "))
                 # 2) If choice is not in positions list, that means it is played already
@@ -111,90 +118,84 @@ def play_game():
                     choice = choice_validator(input("Pick a cell: "))
                 else:  # choice not played before.
                     positions_list.remove(choice)
-                    # 3) After choice is OK. put the choice on the empty board and store the new board in the same variable.
+                    # 3) After choice is OK. put the choice on the empty board and store the new board in
+                    # the same variable.
                     new_board = put_symbol_on_board(choice, "X", new_board)
                     print("One", new_board)  # Showing the user his symbol on the board.
+
                     # 4) In this cycle in the game, if that choice made a win pattern complete, game will be false and
-                    # the cycle will break skipping the computer choice. when the while loop checks "game",
-                    # it will find it "False" and will not enter into a new game.
+                    # the cycle will break skipping the computer choice. When the next lines check if cycle_finished
+                    # is still false, it will find it true and will check game which will be False and the game will end
                     if win(new_board):
                         print("You Won")
                         game = False
                         cycle_finished = True
-                        check_play_again = input("Do you want to play again? (Y/N) : ")
-                        while check_play_again.upper() not in ("Y", "N"):
-                            check_play_again = input("Do you want to play again? (Y/N) : ").upper()
-                        if check_play_again == "N":
-                            print ("N")
-                            yes = False
-                        else:
-                            yes=True
-
 
                     else:  # meaning the user choice didn't make him win.
+                        time.sleep(1)
                         if not cycle_finished:
                             positions_list = positions_list  # update positions list after removing the previous users
                             # choice
-                            # 5) Check if the user picked the last cell in the board which corresponds to an empty positions
-                            # list. If so, then it is a tie. If this choice was a winning pattern, the game would have ended
-                            # on line 115.
+                            # 5) Check if the user picked the last cell in the board which corresponds to an
+                            # empty positions list. If so, then it is a tie. If this choice was a winning pattern,
+                            # the game would have ended on line 128.
                             if len(positions_list) == 0:
                                 print("       TIE    ")
                                 game = False
-                                break
+                                cycle_finished = True
                             else:  # There are still numbers not picked.
-
-                                # 6) Where should the computer put the next "O"?
-                                # Firstly : The program will loop over each position left and put an "O" in that position.
-                                for position in positions_list:
-                                    fict_ai_board_two = put_symbol_on_board(position, "O", new_board)
-                                    # If the resulting board has a winning pattern, the board will be updated like that
-                                    # and the game will finish as the computer has won. else (line 142), continue to the
-                                    # next position.
-                                    if win(fict_ai_board_two):
-                                        cycle_finished = True
-                                        game = False
-                                        new_board = put_symbol_on_board(position, "O", new_board)
-                                        time.sleep(1)
-                                        print (new_board)
-                                        time.sleep(1)
-                                        print("You Lost")
-                                        check_play_again = input("Do you want to play again? (Y/N) : ")
-                                        while check_play_again.upper() not in ("Y", "N"):
-                                            check_play_again = input("Do you want to play again? (Y/N) : ").upper()
-                                        if check_play_again == "N":
-                                            print("N")
-                                            yes = False
-                                        else:
-                                            yes = True
-                                        break
-                                    else:  # This position will not result in a winning pattern.
-                                        continue
-                                if not game:  # After finishing looping over all positions, check if game is still True. if not
+                                if game:
+                                    # 6) Where should the computer put the next "O"?
+                                    # Firstly : The program will loop over each position left and put an "O" in
+                                    # that position.
+                                    for position in positions_list:
+                                        fict_ai_board_two = put_symbol_on_board(position, "O", new_board)
+                                        # If the resulting board has a winning pattern, the board will be updated like
+                                        # that and the game will finish as the computer has won. else (line 164),
+                                        # continue to the next position.
+                                        if win(fict_ai_board_two):
+                                            cycle_finished = True
+                                            game = False
+                                            new_board = put_symbol_on_board(position, "O", new_board)
+                                            # time.sleep(1)
+                                            print(new_board)
+                                            time.sleep(1)
+                                            print("You Lost")
+                                            break  # the for loop. Stop trying other positions.
+                                        else:  # This position will not result in a winning pattern.
+                                            continue
+                                    # After finishing looping over all positions, check if game is still True. if not
                                     # break the cycle and the game is finished.
-                                    break
-                                # Secondly: The program will loop again over each position but this time it will put an "X"
-                                # in the position and see if the user will win if the user put "X" in the next cycle.
-                                # If so, the computer will put an "O" in that position.
-                                for position in positions_list:
-                                    fict_ai_board = put_symbol_on_board(position, "X", new_board)
-                                    if win(fict_ai_board):  # if this position will make the user win
-                                        new_board = put_symbol_on_board(position, "O", new_board)
-                                        positions_list.remove(position)
-                                        cycle_finished = True
-                                        break
-                                    else:
-                                        continue
-                                # After the 2 loops finished and still no "O' was put (cycle_finished still False)
-                                # then choose a random position from positions list and put "O" in it.
-                                if not cycle_finished:
-                                    comp_choice = random.choice(positions_list)
-                                    new_board = put_symbol_on_board(comp_choice, "O", new_board)
-                                    positions_list.remove(comp_choice)
-                                #
-                                time.sleep(1)
-                                # if win(new_board):
-                                #     game = False
+
+                                    if game:
+                                        # Secondly: The program will loop again over each position but this time it will
+                                        # put an "X" in the position and see if the user will win if the user put "X" in
+                                        # the next cycle.
+                                        # If so, the computer will put an "O" in that position.
+                                        for position in positions_list:
+                                            fict_ai_board = put_symbol_on_board(position, "X", new_board)
+                                            if win(fict_ai_board):  # if this position will make the user win
+                                                new_board = put_symbol_on_board(position, "O", new_board)
+                                                positions_list.remove(position)
+                                                cycle_finished = True
+                                                break
+                                            else:
+                                                continue
+                                        # After the 2 loops finished and still no "O' was put (cycle_finished still
+                                        # False) then, choose a random position from positions list and put "O" in it.
+                                        if not cycle_finished:
+                                            comp_choice = random.choice(positions_list)
+                                            new_board = put_symbol_on_board(comp_choice, "O", new_board)
+                                            positions_list.remove(comp_choice)
+        #  If for any reason, game is False and its while loop finished looping, then before going to the uppermost
+        # while loop(yes loop) ask the user if he wants to play again. Then check the "While yes" loop.
+        check_play_again = input("Do you want to play again? (Y/N) : ").upper()
+        while check_play_again.upper() not in ("Y", "N"):
+            check_play_again = input("Do you want to play again? (Y/N) : ").upper()
+        if check_play_again == "N":
+            yes = False
+        else:
+            yes = True
 
 
 play_game()
